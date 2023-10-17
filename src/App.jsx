0c6fd26a5ae33fbd588e75226ce8cfbcf9d5eddd
Indra <indra@digitizeco.in>
2023-10-17 14:18:14 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { ThreekitProvider } from '@threekit-tools/treble';
 import './index.css'
 import Main from './components/Main';
-
+import Icon from './components/Header';
 const App = () => {
   return (
     <>
+    
       <ThreekitProvider>
+      <Icon/>
         <Main />
         
       </ThreekitProvider>
